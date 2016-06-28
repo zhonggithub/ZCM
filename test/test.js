bf1a6dd63904a4c9cca8fc10705db4372d887530
zhaozhi <zhaozhi@mesada.com.cn>
2016-06-28 09:50:08 +0800

    修改测试文件

--- test/test.js
+++ test/test.js
@@ -3,155 +3,25 @@
  * FileName:  test.js
  * Author: Zz
  * Version: 1.0.0
- * Date: 2016/6/19  10:15
+ * Date: 2016/6/28  9:42
  * Description:
  */
-'use strict'
-var util = require('./../lib/utils');
-var ZControl = require('../index').ZControl;
-var ZElement = require('../index').ZElement;
 
-var uuid = Date.now().toString();
-class DBClass{
-    constructor(logicDB){
-        this.uuid = !logicDB.uuid ? uuid : logicDB.uuid;
-        this.customData = logicDB.customData;
-    }
-}
-class LogicClass{
-    constructor(dbInfo){
-        this.uuid = dbInfo.uuid;
-        this.customData = dbInfo.customData;
-        this.createAt = dbInfo.createAt;
-        this.modifiedAt = dbInfo.modifiedAt;
+    'use strict';
+class Person{
+    constructor(name){
+        this.name = name;
     }
 }
 
-function isValidParams(info){
-    var ret = {is:true, error:''};
-    ret.error = util.mandatoryParams(info, [ 'customData']);
-    if(ret.error)
-    {
-        ret.is = false;
-        return ret;
-    }
-    return ret;
-}
-function isExpandStrValid(expandStr)
-{
-    var expandArray = expandStr.split(',');
-    for(var i = 0; i < expandArray.length; ++i) {
-        switch (expandArray[i]) {
-            case 'tenant':case 'directory':
-            break;
-            default:
-                return false;
-        }
-    }
-    return true;
+function fun(name){
+    name = name;
 }
 
-function isValidQueryCondition(queryCondition) {
-    for(var item in queryCondition)
-    {
-        switch(item)
-        {
-            case 'createAt' : case 'modifiedAt' :case 'expand': case 'uuid':
-            case 'offset': case 'limit':case 'status':case 'orderBy':
-            break;
-            default:
-                return false;
-        }
-    }
-    return true;
-}
 
-var element = new ZElement('CustomData', isValidParams, isValidQueryCondition, isExpandStrValid, DBClass, LogicClass);
+var name = 'Zz';
+var p = new Person(name);
+fun(name);
+name = 'hello world';
+console.log(p.name);
 
-var ZControl = require('./../lib/ZControl');
-var control = new ZControl(element, {
-    "client" : "mysql",
-    "connection" : {
-        "host" : "192.168.0.102", //"192.168.6.17",//
-        "user" : "root",
-        "password" : "123456", //"123123",//
-        "database" : "AccountsComponentTestDB",
-        "port" : 3306
-    },
-    "pool" : {
-        "min" : 0,
-        "max" : 7
-    }
-});
-
-uuid = Date.now().toString();
-control.create({customData: "control1 create"}, function(error, result){
-    console.log("=========== control1 create ===============");
-    if(error)
-        console.log(error);
-    else
-        console.log(result);
-});
-
-control.update('uuid', uuid, {customData: 'control1 update'}, function(error, result){
-    console.log("=========== control1 update ===============");
-    if(error)
-        console.log(error);
-    else
-        console.log(result);
-});
-
-control.list({modifiedAt:'(2016-01-14 14:07:02,]', uuid:'{1111frHDRscoqkZbtXP37Q,1466472079424}'}, function(error, result){
-    console.log("=========== control1 list ===============");
-    if(error)
-        console.log(error);
-    else
-        console.log(result);
-});
-
-control.retrieve('uuid', uuid, function(error, result){
-    console.log("=========== control1 retrieve ===============");
-    if(error)
-        console.log(error);
-    else
-        console.log(result);
-});
-
-control.delete('uuid', uuid, function(error, result){
-    console.log("=========== control1 delete ===============");
-    if(error)
-        console.log(error);
-    else
-        console.log(result);
-});
-
-uuid = Date.now().toString();
-var fun = control.createNext({customData: "create next test 251842"}, function(error, result){
-    console.log("=========== create next ===============");
-    if(error)
-        console.log(error);
-    else
-        console.log(result);
-});
-fun.next();
-fun.next();
-
-var updateFun = control.updateNext('uuid', uuid, {customData: 'update next hello 0000 world'}, function(error, result){
-    console.log("=========== update next ===============");
-    if(error)
-        console.log(error);
-    else
-        console.log(result);
-});
-updateFun.next();
-updateFun.next();
-
-var deleteFun = control.deleteNext('uuid', uuid, function(error, result){
-    console.log("=========== delete next ===============");
-    if(error)
-        console.log(error);
-    else
-        console.log(result);
-});
-deleteFun.next();
-deleteFun.next();
